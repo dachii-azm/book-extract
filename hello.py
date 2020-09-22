994871d69067f058c025a8e5e9b150b733419a3a
dachii-azm <alohadaichi@gmail.com>
2020-09-22 20:58:20 +0900

    add Book_Extractor class

--- hello.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread("./sample_image.jpg")
-W=200
-w_ratio=200/img.shape[1]
-img = cv2.resize(img,(200, int(w_ratio*img.shape[0])))
-
-print(img)
---
+++ run.py
@@ -0,0 +1,11 @@
+from book_extractor2 import Book_Extractor
+import sys
+
+args = sys.argv
+
+if(2<= len(args)):
+    img_name = str(args[1])
+    book_extractor = Book_Extractor(img_name)
+    book_extractor.run()
+else:
+    print("Argument Error")
